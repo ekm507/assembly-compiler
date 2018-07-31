3306f08f91e4d540e9a3a2913df94b57f3ca39af
Erfan Kheyrollahi <ekm507@gmail.com>
2018-08-01 01:13:21 +0430

    now code array is dynamically allocated.

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "compiler.h"
 
@@ -25,10 +26,18 @@ int main(int argc, char**argv)
 	else
 		strcpy(outputfilename,  argv[2]);
 
-	//load file into the memory
+	//open the code file
 	FILE* codefile;
 	codefile = fopen(filename, "r");
-	char code[10000];
+
+	unsigned long int size;
+
+	fseek(codefile, 0, SEEK_END); // seek to end of file
+	size = ftell(codefile); // get current file pointer
+	fseek(codefile, 0, SEEK_SET); // seek back to beginning of file
+
+	char *code;
+	code = malloc(size * sizeof(char));
 	unsigned long int i = 0;
 	while(!feof(codefile))
 		code[++i] = fgetc(codefile);
